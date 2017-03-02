11de5fee3302ba9e521b95b758c197ad3be1f77f
santosh s c <san.461@gmail.com>
2017-03-02 13:59:34 +0530

    String assignment1

---
+++ StringsAssignment1.java
@@ -0,0 +1,14 @@
+package Assignment1;
+
+public class StringsAssignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x= 50;
+		String s = String.valueOf(x);
+		System.out.println(s);
+		
+	}
+
+}
